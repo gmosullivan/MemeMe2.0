5a075b016833ec7e482470692c6c2e4939dafbc1
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 07:51:41 +0100

    Initial commit - set up Meme struct

---
+++ MemeMe2.0/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  MemeMe2.0
+//
+//  Created by Gareth O'Sullivan on 18/07/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK :- Meme Struct
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    var memedImage: UIImage
+}
