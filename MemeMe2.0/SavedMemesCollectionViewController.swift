abd9984082c2a1d316baf1eb5a4e60f50d06b4e6
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 08:13:34 +0100

    Set up collection view controller

---
+++ MemeMe2.0/SavedMemesCollectionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SavedMemesCollectionViewController.swift
+//  MemeMe2.0
+//
+//  Created by Gareth O'Sullivan on 18/07/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class SavedMemesCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+}
