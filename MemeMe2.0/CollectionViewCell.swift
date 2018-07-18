00bbb92bb85e31c1929e9f917998fe341cbcdda8
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 20:06:15 +0100

    Set up CollectionViewCell with image view and constraints

---
+++ MemeMe2.0/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe2.0
+//
+//  Created by Gareth O'Sullivan on 18/07/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+}
