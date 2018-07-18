f6cb4d595dee896c124654f0f5556cff26cdaa77
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 18:02:13 +0100

    Added MemeDetailViewController

---
+++ MemeMe2.0/MemeDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe2.0
+//
+//  Created by Gareth O'Sullivan on 18/07/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
