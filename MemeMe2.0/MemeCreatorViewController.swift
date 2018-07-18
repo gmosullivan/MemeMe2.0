a6ed507624005b8b4b4dbe41ad9fc9b59be7d669
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 08:04:59 +0100

    Renamed view controller to MemeCreatorViewController

---
+++ MemeMe2.0/MemeCreatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MemeMe2.0
+//
+//  Created by Gareth O'Sullivan on 18/07/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class MemeCreatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
