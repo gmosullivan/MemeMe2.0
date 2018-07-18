42c4e2df7556f43cebc418f88af356d37aec549f
garethosullivan <gareth.osullivan@icloud.com>
2018-07-18 18:17:01 +0100

    Added reuse identifier for table view cell

--- MemeMe2.0/SavedMemesTableViewController.swift
+++ MemeMe2.0/SavedMemesTableViewController.swift
@@ -23,7 +23,7 @@ class SavedMemesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Saved Meme Cell", for: indexPath)
 
         // Configure the cell...
 
