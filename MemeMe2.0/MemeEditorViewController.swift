33bd811c20c61e85021b3f76e2721d2c2a471636
garethosullivan <gareth.osullivan@icloud.com>
2018-07-23 21:18:42 +0100

    Tested segue with print statement. Removed print statement

--- MemeMe2.0/MemeEditorViewController.swift
+++ MemeMe2.0/MemeEditorViewController.swift
@@ -15,7 +15,6 @@ class MemeEditorViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.title = "Edit Meme"
-        print(memeToEdit)
     }
     
 }
