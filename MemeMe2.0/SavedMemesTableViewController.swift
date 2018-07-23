07d61b7262134e9bd507ff54f2b5a748c0d9b6a9
garethosullivan <gareth.osullivan@icloud.com>
2018-07-23 19:59:09 +0100

    Reloaded table data

--- MemeMe2.0/SavedMemesTableViewController.swift
+++ MemeMe2.0/SavedMemesTableViewController.swift
@@ -18,6 +18,8 @@ class SavedMemesTableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        tableView.reloadData()
+        tabBarController?.tabBar.isHidden = false
     }
 
     // MARK: - Table view data source
